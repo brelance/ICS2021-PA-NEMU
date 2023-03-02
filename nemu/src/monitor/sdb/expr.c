b71cd18f56ee9c7bc6c75d5e2a0ea8ba82d5fd6f
tracer-ics2021 <tracer@njuics.org>
2023-03-01 19:42:57 -0800

    >  compile brelance82@gmail.com    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 7849c0689601495e83039ad489d24ebd            Boot ID: f5b5df1a8e6d46d7bd7efa8dd7508634     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-60-generic       Architecture: x86-64  19:42:57 up 23:36,  1 user,  load average: 0.10, 0.05, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -53,7 +53,7 @@ typedef struct token
 } Token;
 
 static Token tokens[32] __attribute__((used)) = {};
-static int nr_token __attribute__((used)) = 0;
+static int nr_token = 0;
 
 void print_tokne(Token *t)
 {
@@ -252,8 +252,8 @@ uint64_t eval(int p, int q)
         uint64_t reg = isa_reg_str2val(tokens[p].str, &success);
         sprintf(tokens[p].str, "%lu", reg);
       }
+
       uint64_t num = strtoul(tokens[p].str, NULL, 10);
-      printf("%lu\n", num);
       switch (tokens[p + 1].type)
       {
       case TK_DREF:
