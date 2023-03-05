6ec9426a630d89eb4ad46c3c98933476989b16b6
tracer-ics2021 <tracer@njuics.org>
2023-03-04 22:57:45 -0800

    >  compile brelance82@gmail.com    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 7849c0689601495e83039ad489d24ebd            Boot ID: f5b5df1a8e6d46d7bd7efa8dd7508634     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-60-generic       Architecture: x86-64  22:57:45 up 1 day, 19:03,  1 user,  load average: 0.15, 0.13, 0.06

--- nemu/src/isa/riscv64/instr/decode.c
+++ nemu/src/isa/riscv64/instr/decode.c
@@ -29,6 +29,7 @@ static inline def_DHelper(I) {
 }
 
 static inline def_DHelper(U) {
+  printf("立即数: %x\n", s->isa.instr.u.simm31_12);
   decode_op_i(s, id_src1, (sword_t)s->isa.instr.u.simm31_12 << 12, true);
   decode_op_r(s, id_dest, s->isa.instr.u.rd, true);
 }
