23ddc928d5320a18dec379390e6c5fbbe1890653
tracer-ics2021 <tracer@njuics.org>
2023-03-05 01:07:23 -0800

    >  compile brelance82@gmail.com    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: 7849c0689601495e83039ad489d24ebd            Boot ID: f5b5df1a8e6d46d7bd7efa8dd7508634     Virtualization: vmware   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-60-generic       Architecture: x86-64  01:07:23 up 1 day, 21:13,  1 user,  load average: 0.18, 0.06, 0.02

--- nemu/src/isa/riscv64/instr/decode.c
+++ nemu/src/isa/riscv64/instr/decode.c
@@ -1,8 +1,10 @@
 #include "../local-include/reg.h"
 #include <cpu/ifetch.h>
 #include <isa-all-instr.h>
+#include "../include/isa-def.h"
 
 def_all_THelper();
+
 
 static uint32_t get_instr(Decode *s) {
   return s->isa.instr.val;
@@ -22,14 +24,24 @@ static inline def_DopHelper(r) {
   op->preg = (is_write && val == 0) ? &zero_null : &gpr(val);
 }
 
+static inline def_DopHelper(j) {
+  op->imm = 0;
+  op->imm |= s->isa.instr.j.simm20 << 20;
+  op->imm |= s->isa.instr.j.simm19_12 << 12;
+  op->imm |= s->isa.instr.j.simm11 << 11;
+  op->imm |= s->isa.instr.j.simm10_1 << 1;
+  printf("def_DopHelper: %lx", op->imm);
+}
+
+
 static inline def_DHelper(I) {
   decode_op_r(s, id_src1, s->isa.instr.i.rs1, false);
   decode_op_i(s, id_src2, (sword_t)s->isa.instr.i.simm11_0, false);
   decode_op_r(s, id_dest, s->isa.instr.i.rd, true);
 }
 
+
 static inline def_DHelper(U) {
-  printf("立即数: %x\n", s->isa.instr.u.simm31_12);
   decode_op_i(s, id_src1, (sword_t)s->isa.instr.u.simm31_12 << 12, true);
   decode_op_r(s, id_dest, s->isa.instr.u.rd, true);
 }
@@ -39,6 +51,11 @@ static inline def_DHelper(S) {
   sword_t simm = (s->isa.instr.s.simm11_5 << 5) | s->isa.instr.s.imm4_0;
   decode_op_i(s, id_src2, simm, false);
   decode_op_r(s, id_dest, s->isa.instr.s.rs2, false);
+}
+
+static inline def_DHelper(J) {
+  decode_op_j(s, id_src1, 0, true);
+  decode_op_r(s, id_dest, s->isa.instr.j.rd, true);
 }
 
 def_THelper(load) {
@@ -61,7 +78,7 @@ def_THelper(main) {
   def_INSTR_IDTAB("??????? ????? ????? ??? ????? 01000 11", S     , store);
   def_INSTR_IDTAB("??????? ????? ????? ??? ????? 00101 11", U     , auipc);
   def_INSTR_IDTAB("??????? ????? ????? ??? ????? 00100 11", I     , compute);
-  def_INSTR_IDTAB("??????? ????? ????? ??? ????? 11011 11", U     , jal);
+  def_INSTR_IDTAB("??????? ????? ????? ??? ????? 11011 11", J     , jal);
   def_INSTR_IDTAB("??????? ????? ????? ??? ????? 11001 11", I     , jalr);
 
   def_INSTR_TAB  ("??????? ????? ????? ??? ????? 11010 11",         nemu_trap);
