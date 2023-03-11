61e530f68426415aa807ec71c2aa1a5c34cb7948
brelance <brelance82@gmail.com>
2023-03-10 20:26:38 -0800

    Infrastructure: Add mtracer

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -3,34 +3,38 @@
 #include <device/mmio.h>
 #include <isa.h>
 
-#if   defined(CONFIG_TARGET_AM)
+#if defined(CONFIG_TARGET_AM)
 static uint8_t *pmem = NULL;
 #else
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 // NEMU memory to host memory
-uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
+uint8_t *guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 // host memory to NEMU memory
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
-static word_t pmem_read(paddr_t addr, int len) {
+static word_t pmem_read(paddr_t addr, int len)
+{
   word_t ret = host_read(guest_to_host(addr), len);
   return ret;
 }
 
-static void pmem_write(paddr_t addr, int len, word_t data) {
+static void pmem_write(paddr_t addr, int len, word_t data)
+{
   host_write(guest_to_host(addr), len, data);
 }
 
-void init_mem() {
-#if   defined(CONFIG_TARGET_AM)
+void init_mem()
+{
+#if defined(CONFIG_TARGET_AM)
   pmem = malloc(CONFIG_MSIZE);
   assert(pmem);
 #endif
 #ifdef CONFIG_MEM_RANDOM
   uint32_t *p = (uint32_t *)pmem;
   int i;
-  for (i = 0; i < (int) (CONFIG_MSIZE / sizeof(p[0])); i++) {
+  for (i = 0; i < (int)(CONFIG_MSIZE / sizeof(p[0])); i++)
+  {
     p[i] = rand();
   }
 #endif
@@ -38,16 +42,30 @@ void init_mem() {
       (paddr_t)CONFIG_MBASE, (paddr_t)CONFIG_MBASE + CONFIG_MSIZE);
 }
 
-word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+word_t paddr_read(paddr_t addr, int len)
+{
+#ifdef CONFIG_MTRACE
+  log_write("memory tracer read: address = " FMT_PADDR " read_lenth = %d \n", addr, len);
+#endif
+
+  if (likely(in_pmem(addr)))
+    return pmem_read(addr, len);
   MUXDEF(CONFIG_DEVICE, return mmio_read(addr, len),
-    panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
-      addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
+         panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
+               addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
 }
 
-void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
+void paddr_write(paddr_t addr, int len, word_t data)
+{
+#ifdef CONFIG_MTRACE
+  log_write("memory tracer write: address = " FMT_PADDR " write_lenth = %d \n", addr, len);
+#endif
+  if (likely(in_pmem(addr)))
+  {
+    pmem_write(addr, len, data);
+    return;
+  }
   MUXDEF(CONFIG_DEVICE, mmio_write(addr, len, data),
-    panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
-      addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
+         panic("address = " FMT_PADDR " is out of bound of pmem [" FMT_PADDR ", " FMT_PADDR ") at pc = " FMT_WORD,
+               addr, CONFIG_MBASE, CONFIG_MBASE + CONFIG_MSIZE, cpu.pc));
 }
